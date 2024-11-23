6befddabb6d3838b23c3de1893889db480e9bc70
Joel <joelmdbird@gmail.com>
2024-11-23 15:12:14 +0200

    first commit

--- src/components/DiscordLogin.tsx
+++ src/components/DiscordLogin.tsx
@@ -4,6 +4,8 @@ import { Button } from "@chakra-ui/react";
 const DiscordLogin: React.FC = () => {
   const handleLogin = () => {
     const apiUrl = process.env.REACT_APP_API_URL;
+    console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    console.log(apiUrl);
     window.location.href = `${apiUrl}/auth/discord`;
   };
 

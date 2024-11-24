7174edf86964332b4dbd71385e43612aa7b7a9a0
Joel <joelmdbird@gmail.com>
2024-11-24 14:39:56 +0200

    .

--- src/components/DiscordLogin.tsx
+++ src/components/DiscordLogin.tsx
@@ -4,8 +4,7 @@ import { Button } from "@chakra-ui/react";
 const DiscordLogin: React.FC = () => {
   const handleLogin = () => {
     const apiUrl = process.env.REACT_APP_API_URL;
-    const redirectUri = `https://discord.com/api/oauth2/authorize?client_id=${process.env.DISCORD_CLIENT_ID}&redirect_uri=${process.env.DISCORD_REDIRECT_URI}&response_type=code&scope=identify`;
-    window.location.href = redirectUri;
+    window.location.href = `${apiUrl}/auth/discord`;
   };
 
   return <Button onClick={handleLogin}>Login with Discord</Button>;
